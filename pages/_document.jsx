763e9752125788eb87be99f6a8eb5450f7857440
Jason Banahan <banahanjason@gmail.com>
2022-11-06 16:36:51 -0600

    Update to last version in book

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,71 +5,21 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import createCache from '@emotion/cache';
 
-class MyDocument extends Document {
-  static getInitialProps = async (ctx) => {
-    // Render app and page and get the context of the page with collected side effects.
-    const originalRenderPage = ctx.renderPage;
+function MyDocument() {
+  return (
+    <Html lang="en" style={{ height: '100%' }}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="google" content="notranslate" />
+        <meta name="theme-color" content="#1976D2" />
 
-    // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
-    // However, be aware that it can have global side effects.
-    const cache = createCache({
-      key: 'css',
-      prepend: true,
-    });
-    const { extractCriticalToChunks } = createEmotionServer(cache);
+        <link rel="shortcut icon" href="https://storage.googleapis.com/builderbook/favicon32.png" />
 
-    ctx.renderPage = () =>
-      originalRenderPage({
-        // eslint-disable-next-line react/display-name
-        enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
-      });
+        <link rel="stylesheet" href="/fonts/server.css" />
+        <link rel="stylesheet" href="https://storage.googleapis.com/builderbook/vs.min.css" />
 
-    const initialProps = await Document.getInitialProps(ctx);
-    // This is important. It prevents emotion to render invalid HTML.
-    // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
-    const emotionStyles = extractCriticalToChunks(initialProps.html);
-    const emotionStyleTags = emotionStyles.styles.map((style) => (
-      <style
-        data-emotion={`${style.key} ${style.ids.join(' ')}`}
-        key={style.key}
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: style.css }}
-      />
-    ));
-
-    return {
-      ...initialProps,
-      // Styles fragment is rendered after the app and page rendering finish.
-      styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
-    };
-  };
-
-  render() {
-    return (
-      <Html lang="en" style={{ height: '100%' }}>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="google" content="notranslate" />
-          <meta name="theme-color" content="#1976D2" />
-
-          <link
-            rel="shortcut icon"
-            href="https://storage.googleapis.com/builderbook/favicon32.png"
-          />
-
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400:latin"
-          />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-          <link
-            rel="stylesheet"
-            href="https://storage.googleapis.com/builderbook/nprogress.min.css"
-          />
-          <link rel="stylesheet" href="https://storage.googleapis.com/builderbook/vs.min.css" />
-
-          <style>
-            {`
+        <style>
+          {`
               a {
                 font-weight: 400;
                 color: #58a6ff;
@@ -95,61 +45,68 @@ class MyDocument extends Document {
                 background: #FFF;
               }
             `}
-          </style>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        </style>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
 
-// MyDocument.getInitialProps = async (ctx) => {
-//   // Resolution order
-//   //
-//   // On the server:
-//   // 1. app.getInitialProps
-//   // 2. page.getInitialProps
-//   // 3. document.getInitialProps
-//   // 4. app.render
-//   // 5. page.render
-//   // 6. document.render
-//   //
-//   // On the server with error:
-//   // 1. document.getInitialProps
-//   // 2. app.render
-//   // 3. page.render
-//   // 4. document.render
-//   //
-//   // On the client
-//   // 1. app.getInitialProps
-//   // 2. page.getInitialProps
-//   // 3. app.render
-//   // 4. page.render
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const originalRenderPage = ctx.renderPage;
 
-//   // Render app and page and get the context of the page with collected side effects.
-//   const sheets = new ServerStyleSheets();
-//   const originalRenderPage = ctx.renderPage;
+  // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
+  const cache = createCache({
+    key: 'css',
+    prepend: true,
+  });
+  const { extractCriticalToChunks } = createEmotionServer(cache);
 
-//   ctx.renderPage = () =>
-//     originalRenderPage({
-//       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-//     });
+  ctx.renderPage = () =>
+    originalRenderPage({
+      // eslint-disable-next-line react/display-name
+      enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
+    });
 
-//   const initialProps = await Document.getInitialProps(ctx);
+  const initialProps = await Document.getInitialProps(ctx);
+  // This is important. It prevents emotion to render invalid HTML.
+  // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
 
-//   return {
-//     ...initialProps,
-//     // Styles fragment is rendered after the app and page rendering finish.
-//     styles: (
-//       <React.Fragment>
-//         {initialProps.styles}
-//         {sheets.getStyleElement()}
-//       </React.Fragment>
-//     ),
-//   };
-// };
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
+  };
+};
 
 export default MyDocument;
